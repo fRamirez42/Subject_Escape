8aa8b6feeb0278a007cd6795dcc06e3689ff56bc
Foxyz24 <feliperamirez2004@gmail.com>
2024-08-15 11:55:37 -0400

    Explosion works and is set

--- Subject Escape/Assets/Scripts/ContactAction.cs
+++ Subject Escape/Assets/Scripts/ContactAction.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class ContactAction : MonoBehaviour
 {
+    public GameObject explosion;  // Reference to the explosion prefab
+    private bool isPlayerInCollider = false;
+    public Vector3 explosionPosition = new Vector3(-1800, 121, -2010); // Desired explosion position
+    private int counter = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        counter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if the player is in the collider
+        if (isPlayerInCollider && counter == 0)
+        {
+            // Instantiate the explosion at the specified position
+            Instantiate(explosion, explosionPosition, Quaternion.identity);
+
+            // Reset the flag so the explosion only happens once
+            isPlayerInCollider = false;
+
+            counter++;
+        }
     }
+
+    // Called when the player enters the collider
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "ExplosionSetOff")
+        {
+            isPlayerInCollider = true;
+            Debug.Log("Player entered collider");
+        }
+    }
+
+
 }
