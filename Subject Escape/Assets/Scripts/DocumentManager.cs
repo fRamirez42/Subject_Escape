b85a6144d2a656761868eac5eb5648257a467c36
Foxyz24 <feliperamirez2004@gmail.com>
2024-09-07 11:38:57 -0400

    Working on domcuments

--- Subject Escape/Assets/Scripts/DocumentManager.cs
+++ Subject Escape/Assets/Scripts/DocumentManager.cs
@@ -1,33 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DocumentManager : MonoBehaviour
 {
     public StressManager documentstress;
     public MeshRenderer PRender;
     private bool isPlayerInCollider = false;
+    private bool isDocumentVisible = false; // Track if document is visible
+    public TextMeshProUGUI doc1;
+    public MeshCollider PRcollider;
 
     // Start is called before the first frame update
     void Start()
     {
         PRender.enabled = true;
+        doc1.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check if the 'E' key is pressed and the player is in the collider
+        // Check if the player is in the collider and if the 'E' key is pressed
         if (isPlayerInCollider && Input.GetKeyDown(KeyCode.E))
         {
-            documentstress.flashbackReading();
+            // Toggle the document's visibility
+            isDocumentVisible = !isDocumentVisible;
 
-            // Disable the MeshRenderer
-            PRender.enabled = false;
-            Debug.Log("E key pressed while in collider, MeshRenderer disabled");
-            
+            // If the document is now visible, enable the document, disable MeshRenderer and MeshCollider
+            if (isDocumentVisible)
+            {
+                doc1.gameObject.SetActive(true);
+                documentstress.flashbackReading();
+                PRender.enabled = false;
+                PRcollider.enabled = false;
+                Debug.Log("Document is now visible, MeshRenderer disabled");
+            }
+            // If the document is now hidden, disable the document
+            else
+            {
+                doc1.gameObject.SetActive(false);
+                Debug.Log("Document is now hidden");
+            }
         }
     }
+
 
     // Called when the player enters the collider
     void OnTriggerEnter(Collider other)
